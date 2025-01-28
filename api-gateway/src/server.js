43b568abefe546d811e383ec63aefcbe3a33e217
nxtnilesh <nxtnilesh@gmail.com>
2025-01-28 23:04:30 +0530

    ADD : Proxy for Identity service

---
+++ api-gateway/src/server.js
@@ -0,0 +1,64 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import { rateLimit } from "express-rate-limit";
+dotenv.config();
+import proxy from "express-http-proxy";
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 50,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.info(`Rate limit exceeded for IP ${req.ip} for endpoints`);
+    res.status(429).json({
+      success: false,
+      message: "To many requests",
+    });
+  },
+});
+app.use(limiter);
+
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    return req.originalUrl.replace(/^\/v1/, "api");
+  },
+  proxyErrorHanlder: (err, res, next) => {
+    console.log(`Proxy error : ${err.message}`);
+    res.status(500).json({
+      success: false,
+      message: `Internal server error `,
+      err: err.message,
+    });
+  },
+};
+
+// Proxy for auth service
+app.use(
+  "/v1/auth",
+  proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      console.log(
+        `Response received from Identity service: ${proxyRes.statusCode}`
+      );
+      return proxyResData;
+    },
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("API Gateway");
+});
